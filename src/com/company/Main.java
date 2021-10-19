c6ae918fe135730ae7ea3f608215d62c590feff0
fawadhussaini <fawad.hussaini77@gmail.com>
2021-10-19 00:09:59 -0400

    the 3rd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("what is your name?");
+        System.out.println("my name is fawad");
     }
 }
